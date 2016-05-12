49c23b81043ed8c4db0a8a56d5428c4231f432c1
Rodrigo Andrés Díaz Vergara <rdiazv89@gmail.com>
2016-05-12 15:09:34 -0300

    Integrando Redux

--- src/client.js
+++ src/client.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,29 @@ import React, { Component, PropTypes } from 'react';
 import TodoForm from '../TodoForm/TodoForm';
 import TodoList from '../TodoList/TodoList';
 import './App.sass';
+import { connect } from 'react-redux';
+import { addTodo } from '../../redux/todos';
 
-export default class App extends Component {
+class App extends Component {
+  handleAdd = (text) => {
+    console.log(text);
+    this.props.dispatch(addTodo(text));
+  }
+
   render() {
     return (
       <div className="App">
-        <TodoForm />
-        <TodoList />
+        <TodoForm onAdd={this.handleAdd} />
+        <TodoList todos={this.props.allTodos} />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    allTodos: state
+  };
+}
+
+export default connect(mapStateToProps)(App);
